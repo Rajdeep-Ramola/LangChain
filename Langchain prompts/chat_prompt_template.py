088bf451b576b7605f46bf1e5c36f2a63db8dce5
Rajdeep Ramola <rajdeepramola958@gmail.com>
2025-12-06 22:06:31 +0530

    Added LangChain prompts

---
+++ Langchain prompts/chat_prompt_template.py
@@ -0,0 +1,12 @@
+# Using Langchain Chat Prompt Template for multi-turn dynamic prompting
+
+from langchain_core.prompts import ChatPromptTemplate
+
+chat_template = ChatPromptTemplate([
+    ('system', 'You are a helpful {domain} expert'),
+    ('human', 'Explain in simple terms, what is {topic}')
+])
+
+prompt = chat_template.invoke({'domain':'cricket','topic':'Dusra'})
+
+print(prompt)
