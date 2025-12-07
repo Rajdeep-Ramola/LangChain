4c4f6c1f9c54a194d8b0bdda2749295c30e50f01
Rajdeep Ramola <rajdeepramola958@gmail.com>
2025-12-07 21:09:40 +0530

    Added LangChain structured outputs

---
+++ Langchain Structured Outputs/Typeddict.py
@@ -0,0 +1,11 @@
+# Example of TypedDict in Python
+from typing import TypedDict
+
+class Person(TypedDict):
+
+    name: str
+    age: int
+
+new_person: Person = {'name':'nitish', 'age':'35'}
+
+print(new_person)
