38ef645b4d7b637344c2902701189a5818f9ff34
Rajdeep Ramola <rajdeepramola958@gmail.com>
2025-12-15 21:00:19 +0530

    Added LangChain Text Splitters

---
+++ Langchain text splitters/length_based.py
@@ -0,0 +1,17 @@
+# Example of using CharacterTextSplitter to split PDF documents into fixed-length chunks
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader('dl-curriculum.pdf')
+
+docs = loader.load()
+
+splitter = CharacterTextSplitter(
+    chunk_size=200,
+    chunk_overlap=0,
+    separator=''
+)
+
+result = splitter.split_documents(docs)
+
+print(result[1].page_content)
